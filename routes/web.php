54795356aeed716d83d1c7d0a3091f7ce5c61c29
counter-cowboy <79208562003@ya.ru>
2024-03-19 16:46:10 +0300

    docs: bootstrap icons auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('images.index');
+})->name('image.index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
