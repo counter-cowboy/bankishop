4a07f4ccee3b0865caf8e0122d3adbbddabfa523
counter-cowboy <79208562003@ya.ru>
2024-03-19 15:37:14 +0300

    docs: bootstrap, softdeletes

--- database/migrations/2024_03_19_123532_add_column_softdeletes_to_images_table.php
+++ database/migrations/2024_03_19_123532_add_column_softdeletes_to_images_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('images', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('images', function (Blueprint $table) {
+            $table->softDeletes();
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('images');
+        Schema::table('images', function (Blueprint $table) {
+           $table->dropSoftDeletes();
+        });
     }
 };
