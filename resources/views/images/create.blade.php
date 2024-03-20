89380d8d3c10e3adfdf396aad53bfcbfb22696f7
counter-cowboy <79208562003@ya.ru>
2024-03-20 16:24:26 +0300

    feat:bootstrap

--- resources/views/images/create.blade.php
+++ resources/views/images/create.blade.php
@@ -1,51 +1,54 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Create upload</title>
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
 </head>
-<body style="background-color: #bacbe6">
-<section class="container ">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <form method="post"
-                      action="{{route('image.store')}}" enctype="multipart/form-data">
-                    @csrf
 
-                    {{-- image--}}
-                    <div class="form-group w-100 mt-5 mb-5">
-                        <div class="form-label"><label>Add image up to 5 files</label></div>
-                        <div class="input-group">
-                            <div class="custom-file mb-5">
-                                <input type="file" required multiple
-                                       id="multiFile"
-                                       class="custom-file-input" name="images[]">
-                                {{--  Submit button--}}
-                                <input type="submit" class="btn btn-primary" value="Upload">
+<body style="background-color: #bacbe6">
+    <section class="container ">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <form method="post" action="{{route('image.store')}}" enctype="multipart/form-data">
+                        @csrf
+
+                        {{-- image--}}
+                        <div class="form-group w-100 mt-5 mb-5">
+                            <div class="form-label"><label>Add image up to 5 files</label></div>
+                            <div class="input-group">
+                                <div class="custom-file mb-5">
+                                    <input type="file" required multiple id="multiFile" class="custom-file-input"
+                                        name="images[]">
+                                    {{--  Submit button--}}
+                                    <input type="submit" class="btn btn-primary" value="Upload">
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
-                {{--End form--}}
+                    </form>
+                    {{--End form--}}
+                </div>
             </div>
         </div>
-    </div>
-</section>
-{{--End section--}}
-<script>
+    </section>
+    {{--End section--}}
+    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+    <script>
     document.querySelector('#multiFile').addEventListener('change',
-        function () {
+        function() {
             if (this.files.length > 5) {
                 this.value = '';
                 alert('Limit up to 5 files');
             }
         })
-</script>
+    </script>
 </body>
+
 </html>
--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,60 +1,63 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index images</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
 </head>
+
 <body style="background-color: lightgrey">
 
-<section class="container ">
-    <div class="form-group mb-5 mt-5">
-        <form action="{{route('image.create')}}" method="get">
-            <span class="text-danger">You can upload images here</span>
-            <button type="submit" class="btn btn-success">
-                Add images
-            </button>
-        </form>
-    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                @foreach($images as $img)
-                <div class="w-50 mb-5">
-                    <a href="{{asset('storage/images/'. $img->image)}}">
-                        <img src="{{asset('storage/images/'. $img->image ) }}" class="w-50"
-                            alt="preview_image">
-                    </a>
-                    <br>
-                    <div class="text-black">File_ID: {{$img->id}} <br>
-                        Filename: {{$img->image }}<br>
-                        Uploaded at: {{$img->created_at}}<br>
-                        <form action="{{ route('zip', $img->id) }}" method="get">
-                            <button type="submit" class="btn btn-primary">
-                                ZIP-download
-                            </button>
-                        </form>
-                        <hr>
+    <section class="container ">
+        <div class="form-group mb-5 mt-5">
+            <form action="{{route('image.create')}}" method="get">
+                <span class="text-danger">You can upload images here</span>
+                <button type="submit" class="btn btn-success">
+                    Add images
+                </button>
+            </form>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    @foreach($images as $img)
+                    <div class="w-50 mb-5">
+                        <a href="{{asset('storage/images/'. $img->image)}}">
+                            <img src="{{asset('storage/images/'. $img->image ) }}" class="w-50" alt="preview_image">
+                        </a>
+                        <br>
+                        <div class="text-black">File_ID: {{$img->id}} <br>
+                            Filename: {{$img->image }}<br>
+                            Uploaded at: {{$img->created_at}}<br>
+                            <form action="{{ route('zip', $img->id) }}" method="get">
+                                <button type="submit" class="btn btn-primary">
+                                    ZIP-download
+                                </button>
+                            </form>
+                            <hr>
+                        </div>
                     </div>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-{{--<script src="{{asset('assets/js/app.js')}}"></script>--}}
-<script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script>
     document.querySelector('#multiFile').addEventListener('change',
-    function () {
-        if (this.files.length>5){
-            this.value='';
-            alert('Limit up to 5 files');
-        }
-    })
-</script>
+        function() {
+            if (this.files.length > 5) {
+                this.value = '';
+                alert('Limit up to 5 files');
+            }
+        })
+    </script>
 </body>
+
 </html>
