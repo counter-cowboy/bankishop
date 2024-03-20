b15316fd2a07675eff94bad5bc413cafa05162c1
counter-cowboy <79208562003@ya.ru>
2024-03-20 16:37:33 +0300

    feat: final route

--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
-
-Route::get('/', function () {
-    return view('images.index');
-})->name('image.index');
-
-
+ */
 
 Route::get('/', IndexController::class)->name('image.index');
 Route::get('/create', CreateController::class)->name('image.create');
 Route::post('/', StoreController::class)->name('image.store');
 
-
-Route::group(['namespace'=>'\App\Http\Controllers\API', 'prefix'=>'api' ], function ()
-{
+Route::group(['namespace' => '\App\Http\Controllers\API', 'prefix' => 'api'], function () {
     Route::get('/images', \App\Http\Controllers\API\ApiIndexController::class)
-    ->name('api.index');
-Route::get('/images/{image}', \App\Http\Controllers\API\ApiShowController::class)
-    ->name('api.show');
+        ->name('api.index');
+    Route::get('/images/{image}', \App\Http\Controllers\API\ApiShowController::class)
+        ->name('api.show');
 });
-
-
 
 Route::get('/zip/{img}', ZipController::class)->name('zip');
 
